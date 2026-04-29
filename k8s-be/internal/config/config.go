7ade02c14873601a685fe486e593c996b81961cb
amitp07 <amitmp07@gmail.com>
2026-04-30 02:35:04 +0530

    added backend git workflow and config enhanced to fetch props from ssm

--- k8s-be/internal/config/config.go
+++ k8s-be/internal/config/config.go
@@ -1,16 +1,58 @@
 package config
 
-import "os"
+import (
+	"context"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ssm"
+)
 
 type Config struct {
 	NatsURL     string
 	PostgresURL string
 }
 
+const (
+	NATS_URL     = "/cloudcrush/sandbox/nats/url"
+	POSTGRES_URL = "/cloudcrush/sandbox/postgres/url"
+)
+
 func Load() Config {
 	natsUrl := os.Getenv("NATS_DSN")
-
 	pgUrl := os.Getenv("POSTGRES_DSN")
+
+	if natsUrl == "" && pgUrl == "" {
+
+		ctx := context.Background()
+
+		awsConfig, err := config.LoadDefaultConfig(ctx, config.WithRegion("ap-south-1"))
+
+		if err != nil {
+			panic(err)
+		}
+
+		client := ssm.NewFromConfig(awsConfig)
+
+		result, err := client.GetParametersByPath(ctx, &ssm.GetParametersByPathInput{
+			Path:           aws.String("/cloudcrush"),
+			Recursive:      aws.Bool(true),
+			WithDecryption: aws.Bool(true),
+		})
+
+		if err != nil {
+			panic(err)
+		}
+
+		m := map[string]string{}
+		for _, param := range result.Parameters {
+			m[*param.Name] = *param.Value
+		}
+
+		natsUrl = m[NATS_URL]
+		pgUrl = m[POSTGRES_URL]
+	}
 
 	return Config{
 		NatsURL:     natsUrl,
